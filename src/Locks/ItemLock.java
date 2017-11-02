474eea5edf2cd3467b50bf35427b5efe3a6ec37a
Student <student@BurrisDesktop13.dhcp.bsu.edu>
2017-11-02 14:56:25 -0400

    write and quit

--- src/Locks/ItemLock.java
+++ src/Locks/ItemLock.java
@@ -10,22 +10,51 @@ public class ItemLock {
     public String name;
     public String UNLitem;
     public String type;
-    public void ItemLock(String name, String UNLitem, String type){
+    public boolean locked;
+    public void ItemLock(String name, String UNLitem, String type, boolean locked){
         this.name = name;
         this.UNLitem = UNLitem;
         this.type = type;
+        this.locked = locked;
+    }
+    public String getName(){
+        return Name;
     }
     public String getType(){
         return type;
     }
     public String getUNLitem(){
-        return type;
+        return UNLitem;
     }
-    public void Unlock(ItemLock meme){
-        if (meme.getType() == "item"){
-        //scannerboi if the 
-        //    == meme.getUNLitem();
-        //unlock
+    public boolean getlocked(){
+        return locked;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+    public void setUNLitem(String UNLitem){
+        this.UNLitem = UNLitem;
+    }
+    public void setlocked(boolean locked){
+        this.locked = locked;
+    }
+    public void Unlock(ItemLock lock){
+        if (lock.getType() == "item"){
+          if (inventory.contains(lock.getUNLitem())){
+            lock.setlocked(false);
+            System.out.println("Good one pal, it's unlocked");
+          }
+          if (lock.getType() == "String"){
+            
+            
+            
+          }
+          if (lock.getType() == "int"){
+            
+          }
         }
     }
 }
