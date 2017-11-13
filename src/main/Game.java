66f571257c4ed191b58c528929dbc866e7c187b4
Student <student@BurrisDsktop110.dhcp.bsu.edu>
2017-11-13 14:57:38 -0500

    temp

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,21 +1,47 @@
 package main;
 
+import java.util.ArrayList;
+
+import objects.Room;
 
 /**
- * This is the main game class. The function {@link Game#Game.runGame() runGame()} takes commands from the user repeatedly until the game ends.
+ * This is the main game class. The function {@link Game#Game.runGame()
+ * runGame()} takes commands from the user repeatedly until the game ends.
  *
  * @author Robby
  * @version (a version number or a date)
  */
 public class Game {
-    /**
-     * Constructor for objects of class Game
-     */
-    public Game() {
-        // Game
-    }
+	private boolean canDie;
+	private Room deathRoom;
+	private ArrayList<Room> rooms;
+	private Room startingRoom;
 
-    public boolean runGame () {
-        return true;
-    }
+	/**
+	 * Constructor for objects of class Game
+	 */
+	public Game (Room startingRoom) {
+		canDie = false;
+		this.startingRoom = startingRoom;
+	}
+
+	public Game (Room deathRoom, Room startingRoom) {
+		canDie = true;
+		this.deathRoom = deathRoom;
+		this.startingRoom = startingRoom;
+	}
+
+	public boolean runGame () {
+		String cmd = CommandParser.replaceCommand(UserInput.get("What would you like to do"));
+		String cmdAction = cmd.split(" ")[0];
+		if (cmdAction.equals("look")) {
+			// look
+		}
+		while (true) {
+			if (true) {
+				break;
+			}
+		}
+		return true;
+	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,23 +1,25 @@
 package main;
 
+import objects.Room;
+
 /**
  * @author Robby
  *
  */
 public class Main {
-    /**
-     * @param args
-     */
-    public static void main (String [] args) {
-        while (true) {
-            Game game = new Game();
-            boolean win = game.runGame();
-            System.out.print("You ");
-            System.out.println(win ? "won!" : "lost :(");
-            boolean quit = UserInput.getBool("Would you like to play again");
-            if (!quit) {
-                break;
-            }
-        }
-    }
+	/**
+	 * @param args
+	 */
+	public static void main (String [] args) {
+		while (true) {
+			Game game = new Game(new Room());
+			boolean win = game.runGame();
+			System.out.print("You ");
+			System.out.println(win ? "won!" : "lost :(");
+			boolean quit = UserInput.getBool("Would you like to play again");
+			if (!quit) {
+				break;
+			}
+		}
+	}
 }
