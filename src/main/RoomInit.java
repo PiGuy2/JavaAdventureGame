3aa441607123f03259da0634d7a6a8df92cc2e56
student <student@burrisdesktop15.dhcp.bsu.edu>
2017-11-20 14:33:18 -0500

    I made factory and dr. office items

--- src/main/RoomInit.java
+++ src/main/RoomInit.java
@@ -12,28 +12,31 @@ import objects.Room;
 
 /**
  * @author Owen
- * @author Robby
  *
  */
 public class RoomInit {
 	public static Room makeRooms () {
-		ArrayList<InteractiveItem> drOfficeInteractiveItems = new ArrayList<>(
-				Arrays.asList(new InteractiveItem("briefcase", "red"), new InteractiveItem("painting", "pretty"),
-						new InteractiveItem("coat", "large trench"), new InteractiveItem("door", "trap"),
-						new InteractiveItem("dagger", "small"), new InteractiveItem("lamp", ""),
-						new InteractiveItem("piece", "wood")));
-		ArrayList<BackgroundItem> drOfficeBackgroundItems = new ArrayList<>(
-				Arrays.asList(new BackgroundItem("desk", "large"), new BackgroundItem("chair", "comfy"),
-						new BackgroundItem("shelf", "book"), new BackgroundItem("box", "wooden cigar")));
+		ArrayList<InteractiveItem> drOfficeInteractiveItems = new ArrayList<>(Arrays.asList(
+				new InteractiveItem("briefcase", "red"), new InteractiveItem("painting", "pretty"),
+				new InteractiveItem("coat", "large trench"), new InteractiveItem("door", "trap"),
+				new InteractiveItem("dagger", "small"), new InteractiveItem("lamp"),
+				new InteractiveItem("piece", "wood"),
+				new InteractiveItem("card", "ace of spades")));
+		ArrayList<BackgroundItem> drOfficeBackgroundItems = new ArrayList<>(Arrays.asList(
+				new BackgroundItem("desk", "large"), new BackgroundItem("chair", "comfy"),
+				new BackgroundItem("shelf", "book"), new BackgroundItem("box", "wooden cigar")));
 		Room drOffice = new Room("doctor's office", "",
-				"The room is white with windows all around, yet the room seems dark.", drOfficeInteractiveItems,
-				drOfficeBackgroundItems);
+				"The room is white with windows all around, yet the room seems dark.",
+				drOfficeInteractiveItems, drOfficeBackgroundItems);
 		// -----
-		ArrayList<InteractiveItem> factorySInteractiveItems = new ArrayList<>(
-				Arrays.asList(new InteractiveItem("item", "test")));
+		ArrayList<InteractiveItem> factorySInteractiveItems = new ArrayList<>(Arrays.asList(
+				new InteractiveItem("card", "ace of hearts"), new InteractiveItem("can", "trash"),
+				new InteractiveItem("painting", "large"), new InteractiveItem("notebook", "blue"),
+				new InteractiveItem("lock", "pink"), new InteractiveItem("key", "pink")));
 		ArrayList<BackgroundItem> factorySBackgroundItems = new ArrayList<>(
-				Arrays.asList(new BackgroundItem("item", "example")));
-		Room factoryS = new Room("factory", "", "It is cold.", factorySInteractiveItems, factorySBackgroundItems);
+				Arrays.asList(new BackgroundItem("item", "example"), new BackgroundItem("window")));
+		Room factoryS = new Room("factory", "", "It is cold.", factorySInteractiveItems,
+				factorySBackgroundItems);
 		// ___________
 
 		// ArrayList<InteractiveItem> InteractiveItems = new ArrayList<>(Arrays.asList(
